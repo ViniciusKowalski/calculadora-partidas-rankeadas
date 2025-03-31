39f7c0ad8bdb0f16c1c6a3cfc8aacfd71b0c42ce
Vinicius Kowalski <vinicius.kowalski@animati.com.br>
2025-03-31 18:37:00 -0300

    Código do desfio pronto

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
-/*
-Calculadora de partidas Rankeadas
-**O Que deve ser utilizado**
+let namePlayer = "He-man"
+let nivel;
 
-- Variáveis
-- Operadores
-- Laços de repetição
-- Estruturas de decisões
-- Funções
+let level = rankingPlayer(30,10)
+function rankingPlayer(win, loss){
+    let level = win - loss
+    return level
+}
 
-## Objetivo:
-
-Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
-depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
-
-Se vitórias for menor do que 10 = Ferro
-Se vitórias for entre 11 e 20 = Bronze
-Se vitórias for entre 21 e 50 = Prata
-Se vitórias for entre 51 e 80 = Ouro
-Se vitórias for entre 81 e 90 = Diamante
-Se vitórias for entre 91 e 100= Lendário
-Se vitórias for maior ou igual a 101 = Imortal
-
-## Saída
-
-Ao final deve se exibir uma mensagem:
-"O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
-*/
+if (level < 10){
+    nivel = "Ferro"
+}
+else if(level >= 11 && level <= 20){
+     nivel = "Bronze"
+}
+else if(level >= 21 && level <= 50){
+    nivel = "Prata"
+}
+else if(level >= 51 && level <= 80){
+    nivel = "Ouro"
+}
+else if(level >= 81 && level <= 90){
+    nivel = "Diamente"
+}
+else if(level >= 91 && level <= 100){
+    nivel = "Lendário"
+}
+else{
+    nivel = "Imortal"
+}
+switch(nivel){
+    case "Ferro":
+        console.log("O herói " + namePlayer + " tem saldo de " + level + " está no nível de " + nivel)
+    break;
+    case "Bronze":
+        console.log("O herói " + namePlayer + " tem saldo de " + level + " está no nível de " + nivel)
+    break;
+    case "Prata":
+        console.log("O herói " + namePlayer + " tem saldo de " + level + " está no nível de " + nivel)
+    break;
+    case "Ouro":
+        console.log("O herói " + namePlayer + " tem saldo de " + level + " está no nível de " + nivel)
+    break;
+    case "Diamente":
+        console.log("O herói " + namePlayer + " tem saldo de " + level + " está no nível de " + nivel)
+    break;
+    case "Lendário":
+        console.log("O herói " + namePlayer + " tem saldo de " + level + " está no nível de " + nivel)
+    break;
+    case "Imortal":
+        console.log("O herói " + namePlayer + " tem saldo de " + level + " está no nível de " + nivel)
+    break;
+}
